bd33bd921e3f047ed35d7573161a676a920666ec
nantha-dev <nantha1234789@gmail.com>
2025-03-10 15:28:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,11 @@ const express = require('express');
 const serverless = require('serverless-http');
 
 const app = express();
-const router = express.Router();
 
-// Define API route
-router.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.json({ message: 'Hello from Vercel Serverless!' });
 });
 
-app.use('/api', router);
-
-// Export serverless function
+// Export as a serverless function
 module.exports = app;
 module.exports.handler = serverless(app);
